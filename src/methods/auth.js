aafc21a32c1febfa74e7cfc95b32b40c409ad42e
KerimHelal <kareemelsayed1997@gmail.com>
2021-01-11 00:05:49 +0200

    Authentication api requests
    Includes api calls to the  authentication requests like login, register, logout

---
+++ src/methods/auth.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export const register = (email, password, name) => {
+    return axios.post('http://localhost:8080/auth/register', { email, password, name })
+    .then(response => {
+      return response.data.message;
+    });
+}
+
+export const login = (email, password) => {
+    return axios
+      .post('http://localhost:8080/auth/login', { email, password })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem('user', JSON.stringify(response.data));
+        }
+        return response.data;
+      });
+};
+
+export const logout = () => {
+    localStorage.removeItem('user');
+};
+
+export const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem('user'));
+};
+
+export const getAuthHeader = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+  
+    if (user && user.accessToken) {
+      return { Authorization: 'Bearer ' + user.accessToken };
+    } else {
+      return {};
+    }
+  }
+  
